5a89e8e3dc1cf9f0f69989a07c300bcef4b9cd89
Đinh Hoài Nam <namhoanq0108@gmail.com>
2024-05-18 17:57:27 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get("/about-us",function() {
+    return view("/about-us");
+});
